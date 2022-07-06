ca25ecab1fecf902c3b700f80da73aa34f107dbe
Jesus Aguilar <aaguilarvf39@gmail.com>
2022-07-06 01:38:31 -0700

    Board showing color

--- js/main.js
+++ js/main.js
@@ -1,30 +1,28 @@
 /*----- constants -----*/
 const COLORS = {
     '0': 'white',
-    '1': 'blue',
+    '1': 'purple',
     '-1': 'orange'
 };
-
-const WINNING_PATTERNS = {
+const WINNING_COMBOS = {
 
 };
-
 /*----- app's state (variables) -----*/
 // Array of 42 elements?
 // Make the board an array?
 
-let board;  // 42 array where nested arrays rep the columns
+let board;  // 2D array where nested arrays rep the columns
 let turn;  // 1 or -1; 0 for nobddy home in that cell
-let gameStatus;  // null -> game in play; 1/-1 player win; 'S' -> stalemate
+// let gameStatus;  // null -> game in play; 1/-1 player win; 'S' -> stalemate
 
 /*----- cached element references -----*/
 
-const guidesEls = [document.QuerySelectorAll('#guides > div')];
-const messageEls = [];
+const guideEls = [document.querySelectorAll('#guide > div')];
+//   const messageEls = [];
 const replayBtn = document.querySelector('button');
 
 /*----- event listeners -----*/
-document.getElementById('guides').addEventListener('click', handleDrop);
+document.getElementById('guide').addEventListener('click', handleDrop);
 replayBtn.addEventListener('click', init);
 
 /*----- functions -----*/
@@ -33,36 +31,45 @@ init();
 // board = new Array(42).fill(null);
 function init() {
     board = [
-        [0, 0, 0, 0, 0, 0],    // column 0
-        [0, 0, 0, 0, 0, 0],    // column 1
-        [0, 0, 0, 0, 0, 0],    // column 2
-        [0, 0, 0, 0, 0, 0],    // column 3
-        [0, 0, 0, 0, 0, 0],    // column 4
-        [0, 0, 0, 0, 0, 0],    // column 5
-        [0, 0, 0, 0, 0, 0],    // column 6
+      [0, 0, 0, 0, 0, 0],    // column 0
+      [0, 0, 0, 0, 0, 0],    // column 1
+      [0, 0, 0, 0, 0, 0],    // column 2
+      [0, 0, 0, 0, 0, 0],    // column 3
+      [0, 0, 0, 0, 0, 0],    // column 4
+      [0, 0, 0, 0, 0, 0],    // column 5
+      [0, 0, 0, 0, 0, 0],    // column 6
     ];
     turn = 1;
     render();
 }
 
 function render() {
-    // iterate over the column arrays
-    board.forEach(function(colArr, colIdx) {
-        colArr.forEach(function(cellVal, rowIdx) {
-            const cellEl = document.getElementById(`c${colIdx}r${rowIdx}`);
-            cellEl.style.backgroundColor = COLORS[cellVal];
-        });
-    });
+  // iterate over the column arrays
+  board.forEach(function(colArr, colIdx) {
+    colArr.forEach(function(cellVal, rowIdx) {
+       const cellEl = document.getElementById(`c${colIdx}r${rowIdx}`);
+       cellEl.style.backgroundColor = COLORS[cellVal];
+     });
+   });
+    renderGuides();
 }
 
+// hide or show markers
+function renderGuides() {
+    guideEls.forEach(function(guideEl, colIdx) {
+        guideEl.style.visibility = board[colIdx].includes(0) ? 'visible' : 'hidden';
+    });
+}
 // gameStatus = null;
 
 // Update all impacted state, then call render
 function handleDrop(evt) {
-   const colIdx = guidesEls.indexOf(evt.target);
+   const colIdx = guideEls.indexOf(evt.target);
    if (colIdx === -1) return;
    const colArr = board[colIdx];
-
+   const rowIdx = colArr.indexOf(0);
+   colArr[rowIdx] = turn;
+   turn *= -1;
    render();
 }
 //Guards
@@ -75,16 +82,16 @@ function handleDrop(evt) {
 
 // Render's job is to transfer/visualize
 // all state to the DOM
-function render() {
+//     function render() {
 
-}
+//}
 
-function renderMessage() {
+//    function renderMessage() {
     
     // Tie game
 
 
     // Player has won!
-}
+//}
 
 // const audio = new Audio('https://assets.mixkit.co/sfx/preview/mixkit-simple-countdown-922.mp3');
